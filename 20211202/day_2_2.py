9bc1f612e8c8e43ddecebdaeb9810a798650da76
hellboy1975 <hellboy1975@gmail.com>
2021-12-04 09:40:23 +1030

    Successful Day2 Part 2 solution

--- 20211202/day_2_2.py
+++ 20211202/day_2_2.py
@@ -2,25 +2,49 @@ import sys
 sys.path.append('../')
 import shared
 
+print ("""
+                                 o o
+                                 o ooo
+                                   o oo
+                                      o o      |   #)
+                                       oo     _|_|_#_
+                                         o   | U505  |
+    __                    ___________________|       |_________________
+   |   -_______-----------                                              \\
+  >|    _____                                                   --->     )
+   |__ -     ---------_________________________________________________ /
+""")
+
+
 commands = shared.fileToArray('data.txt')
-horizontal = depth = 0
+hPosition = depth = aim = count = 0
+
+# calculate the depth
+def calculateDepth(currentDepth, aim, distance):
+    return currentDepth + (distance * aim)
+
 
 for command in commands:
     action, amount = command.strip().split(' ')
     amount = int(amount)
 
     if (shared.args.debug):
-        print(f'D: {depth} H: {horizontal}')
+        print(f'D: {depth} H: {hPosition} A: {aim}')
         print(f'Command: {action} {amount}')
-    
-    if (action == 'forward'):
-        horizontal += amount
+        if count > 20:
+            break
+        
+        if (action == 'forward'):
+        hPosition += amount
+        depth = calculateDepth(depth, aim, amount)
     elif (action == 'down'):
-        depth += amount     
+        aim += amount     
     else:
-        depth -= amount
+        aim -= amount
+
+    count += 1
 
 print(f'Total commands: {len(commands)}')
 print(f'Final Depth: {depth}')
-print(f'Final Horizontal position: {horizontal}')
-print(f'Final Horizontal * Final Depth =  {horizontal * depth}')
+print(f'Final Horizontal position: {hPosition}')
+print(f'Final Horizontal * Final Depth =  {hPosition * depth}')
